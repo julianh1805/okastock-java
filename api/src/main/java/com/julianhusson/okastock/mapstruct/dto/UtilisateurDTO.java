32127995c3389210ba8600b2f0eb05c6fdae625e
Julianh1805 <husson.j@yahoo.com>
2022-02-22 22:22:27 +0100

    dto classes to record

--- api/src/main/java/com/julianhusson/okastock/mapstruct/dto/UtilisateurDTO.java
+++ api/src/main/java/com/julianhusson/okastock/mapstruct/dto/UtilisateurDTO.java
@@ -1,17 +1,3 @@
 package com.julianhusson.okastock.mapstruct.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class UtilisateurDTO extends AuditableDTO{
-    private String nom;
-    private Long siret;
-    private int codePostal;
-    private Long telephone;
-    private String site;
-    private String logo;
-    private boolean rgpd;
-    private String email;
-}
+public record UtilisateurDTO(String nom, Long siret, int codePostal, Long telephone, String site, String logo, boolean rgpd, String email) {}
