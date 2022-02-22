ffdcfbb60a4f8643811b05547c6870a5c3ee917c
Julianh1805 <husson.j@yahoo.com>
2022-02-22 19:11:24 +0100

    set authorization filter

---
+++ api/src/main/java/com/julianhusson/okastock/mapstruct/mapper/UtilisateurMapper.java
@@ -0,0 +1,10 @@
+package com.julianhusson.okastock.mapstruct.mapper;
+
+import com.julianhusson.okastock.mapstruct.dto.UtilisateurPostDTO;
+import com.julianhusson.okastock.utilisateur.Utilisateur;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface UtilisateurMapper {
+    Utilisateur utilisateurPostDTOToUtilisateur(UtilisateurPostDTO utilisateurPostDTO);
+}
