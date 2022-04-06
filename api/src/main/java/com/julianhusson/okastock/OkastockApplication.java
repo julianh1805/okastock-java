6317061dbfa11be866c47ccfe4bde920eb17faa5
Julianh1805 <husson.j@yahoo.com>
2022-04-06 23:37:56 +0200

    add tests

--- api/src/main/java/com/julianhusson/okastock/OkastockApplication.java
+++ api/src/main/java/com/julianhusson/okastock/OkastockApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 @SpringBootApplication
 @OpenAPIDefinition
@@ -17,6 +21,24 @@ public class OkastockApplication {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ITemplateResolver templateResolver()
+    {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        return templateResolver;
+    }
+
+    @Bean
+    public TemplateEngine templateEngine()
+    {
+        TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(this.templateResolver());
+        return templateEngine;
     }
 
 }
