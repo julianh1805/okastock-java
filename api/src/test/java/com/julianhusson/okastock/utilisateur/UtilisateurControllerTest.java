228c7378668e3000969c348f6a9ef838dca914a4
Julian Husson <43209280+julianh1805@users.noreply.github.com>
2022-05-06 18:54:47 +0200

    Revert "Feature/profil image delete"

--- api/src/test/java/com/julianhusson/okastock/utilisateur/UtilisateurControllerTest.java
+++ api/src/test/java/com/julianhusson/okastock/utilisateur/UtilisateurControllerTest.java
@@ -3,21 +3,18 @@ package com.julianhusson.okastock.utilisateur;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.julianhusson.okastock.configuration.WithMockCustomUser;
 import com.julianhusson.okastock.mapstruct.dto.UtilisateurPostDTO;
-import com.julianhusson.okastock.storage.StorageService;
 import org.hamcrest.core.IsNull;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -33,8 +30,6 @@ class UtilisateurControllerTest {
 
     @Autowired private MockMvc mockMvc;
     @Autowired private ObjectMapper mapper;
-
-    @MockBean private StorageService storageService;
     private final String URI = "/api/v1/users";
 
     @Test
@@ -64,9 +59,8 @@ class UtilisateurControllerTest {
     void itShouldUpdateUser() throws Exception {
         String utilisateurId = "e59ed17d-db7c-4d24-af6c-5154b3f72dfe";
         UtilisateurPostDTO utilisateurPostDTO =
-                new UtilisateurPostDTO("Test 2", 22345678910111L, 44200, 666666662L, "http://www.test2.com", true, "test2@test.com", null);
+                new UtilisateurPostDTO("Test 2", 22345678910111L, 44200, 666666662L, "http://www.test2.com", "-", true, "test2@test.com", null);
         String json = mapper.writeValueAsString(utilisateurPostDTO);
-
         mockMvc.perform(put(URI + "/" + utilisateurId).content(json).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(utilisateurId))
@@ -76,7 +70,7 @@ class UtilisateurControllerTest {
                 .andExpect(jsonPath("$.codePostal").value(44200))
                 .andExpect(jsonPath("$.telephone").value(666666662))
                 .andExpect(jsonPath("$.site").value("http://www.test2.com"))
-                //.andExpect(jsonPath("$.logo").value("-"))
+                .andExpect(jsonPath("$.logo").value("-"))
                 .andExpect(jsonPath("$.rgpd").isBoolean())
                 .andExpect(jsonPath("$.email").value("test2@test.com"))
                 .andExpect(jsonPath("$.motDePasse").doesNotExist());
@@ -86,7 +80,7 @@ class UtilisateurControllerTest {
     void itShouldThrowForbiddenExceptionWhenUpdateUser() throws Exception {
         String utilisateurId = "e59ed17d-db7c-4d24-af6c-5154b3f72dfe";
         UtilisateurPostDTO utilisateurPostDTO =
-                new UtilisateurPostDTO("Test 2", 22345678910111L, 44200, 666666662L, "http://www.test2.com", true, "test2@test.com", null);
+                new UtilisateurPostDTO("Test 2", 22345678910111L, 44200, 666666662L, "http://www.test2.com", "-", true, "test2@test.com", null);
         String json = mapper.writeValueAsString(utilisateurPostDTO);
         mockMvc.perform(put(URI + "/" + utilisateurId).content(json).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isUnauthorized());
@@ -97,7 +91,7 @@ class UtilisateurControllerTest {
     @WithMockCustomUser
     void itShouldDeleteUser() throws Exception {
             String utilisateurId = "e59ed17d-db7c-4d24-af6c-5154b3f72dfe";
-        mockMvc.perform(delete(URI + "/" + utilisateurId))
+            mockMvc.perform(delete(URI + "/" + utilisateurId))
                     .andExpect(status().isNoContent());
     }
 
